1c41308b507d5e7f2832105fcb386e157d4b60df
Daria Isanova <mash.b.work@gmail.com>
2026-08-04 19:00:02 +0500

    fix: поправла интерфейс

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,7 +5,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 interface FilmStorage {
-    Collection<Film> getAll() {}
-    Film create(Film film) {}
-    Film update(Film film) {}
+    Collection<Film> getAll();
+    Film create(Film film);
+    Film update(Film film);
 }
